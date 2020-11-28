c6e60e4dc658584e97759e15730811e29c3c8089
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-11-28 20:25:13 -0300

    Minor adjustments in game logic

--- src/components/Game/renderScreen.js
+++ src/components/Game/renderScreen.js
@@ -1,16 +1,65 @@
-export default function renderScreen(screen, game, requestAnimationFrame) {
+export default function createRenderScreen(
+  screen,
+  game,
+  requestAnimationFrame,
+  currentScore,
+  clearedLines,
+  difficulty,
+  gameTime,
+) {
   const context = screen.getContext('2d');
   context.fillStyle = 'white';
   context.clearRect(0, 0, game.state.width, game.state.height);
 
   for (const tetriminoId in game.state.tetriminos) {
-    game.state.tetriminos[tetriminoId].blocks.forEach((block) => {
+    game.state.tetriminos[tetriminoId].blocks.forEach(block => {
       context.fillStyle = game.state.tetriminos[tetriminoId].color;
       context.fillRect(block.x, block.y, 1, 1);
     });
   }
 
+  if (game.state.is_upsidedown) {
+    screen.style = 'transform: rotate(180deg)';
+  } else {
+    screen.style = 'transform: rotate(0deg)';
+  }
+
+  function updateGameStatus(command) {
+    console.log('updateGameStatus : ');
+    console.log(command);
+    const command_type = command.type;
+    if (command_type === 'update-status-game') {
+      const status_changed = command.status_changed;
+      switch (status_changed) {
+        case 'score':
+          currentScore.innerHTML = game.state.score;
+          break;
+        case 'total_cleared_lines_count':
+          clearedLines.innerHTML = game.state.total_cleared_lines_count;
+          break;
+        case 'time_round':
+          difficulty.innerHTML = game.state.time_round;
+          break;
+        case 'time':
+          gameTime.innerHTML = game.state.time;
+          break;
+      }
+    }
+  }
+
   requestAnimationFrame(() => {
-    renderScreen(screen, game, requestAnimationFrame);
+    createRenderScreen(
+      screen,
+      game,
+      requestAnimationFrame,
+      currentScore,
+      clearedLines,
+      difficulty,
+      gameTime,
+    );
   });
+
+  return {
+    updateGameStatus,
+  };
 }
